093f6288def7355152ed321c4e4233eb2b973e89
subashinibalasubramanian <subashinibalasubramanian@gmail.com>
2022-05-26 11:36:25 +0530

    Added 3 methods

--- src/test/java/com/sample/ClassA.java
+++ src/test/java/com/sample/ClassA.java
@@ -17,4 +17,19 @@ public class ClassA {
 
 	}
 
+	private void method4() {
+		System.out.println("4");
+
+	}
+
+	private void method5() {
+		System.out.println("5");
+
+	}
+
+	private void method6() {
+		System.out.println("6");
+
+	}
+
 }
